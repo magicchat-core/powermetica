1548f4b5e720c768f66494386f7738faacdbb681
codekeep18feb <codekeep18feb@gmail.com>
2025-05-25 19:49:48 +0530

    updated package name

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const isProd = process.env.NODE_ENV === "production";
 console.log("building with process.env.NEXT_PUBLIC_BASE_PATH",process.env.NEXT_PUBLIC_BASE_PATH,process.env.NODE_ENV)
 const nextConfig = {
     output: "export",
-    assetPrefix: isProd ? "/chathead-landing-next" : "",
-    basePath: isProd ? "/chathead-landing-next" : "",
+    assetPrefix: isProd ? "/powermetica" : "",
+    basePath: isProd ? "/powermetica" : "",
     images: { unoptimized: true },
     env: {
-        NEXT_PUBLIC_BASE_PATH: isProd ? "/chathead-landing-next" : "",
+        NEXT_PUBLIC_BASE_PATH: isProd ? "/powermetica" : "",
         NEXT_PUBLIC_API_URL: "https://api.example.com",
     },
 };
